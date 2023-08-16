b9594654acd8f531857a29351e3a7a620268a087
Eduardo Rosa <eduardorosadarosa14@hotmail.com>
2023-08-16 19:22:05 -0300

    Atualização

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -20,29 +20,29 @@ class CaixaDaLanchonete {
         let pagamentos = ["dinheiro", "debito", "credito"];
 
 
-
-
         let total = 0
         let percorre = [];
 
+        //Função para percorrer a lista de Itens
         let listapercorrida = function (percorre, item) {
             return percorre.includes(item)
         }
-
+        //Percorre a lista de Itens e organiza
         for (const itemof of itens) {
             const [item, quant] = itemof.split(",");
             percorre.push(item)
 
 
+        //Verifica modo de Pagamento valido 
             if (!pagamentos.includes(metodoDePagamento)) {
                 return "Forma de pagamento inválida!";
             }            
 
-
+            //Quantida de itens valido para a compra
             if (quant <= 0) {
                 return "Quantidade inválida!";
             }
-
+            //Verificações unitaria
             if (metodoDePagamento === "dinheiro" &&
                 listapercorrida(percorre, "chantily") &&
                 !listapercorrida(percorre, "cafe")) {
@@ -75,6 +75,7 @@ class CaixaDaLanchonete {
                 return 'Item extra não pode ser pedido sem o principal'
               }
 
+              //Percorre os itens faz e soma 
             if (item in this.itens) {
                 const itemValor = this.itens[item].valor * parseInt(quant)
                 total += itemValor
@@ -83,10 +84,11 @@ class CaixaDaLanchonete {
               
             }
         }
+        //verifica de há itens no carrinho
         if (percorre.length == 0) {
             return "Não há itens no carrinho de compra!"
         }
-
+        //Desconto de 5% e Acrescimo de 3%
         if (metodoDePagamento === "dinheiro") {
             total *= 0.95
         } else if (metodoDePagamento === "credito") {
